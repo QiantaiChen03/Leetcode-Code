b4d3090d1189b3410fc0ec999f48d0ce30ff3228
Qiantai <qiantaichen0303@gmail.com>
2021-05-23 20:34:14 -0700

    new array practice

--- src/com/leetcode/ArrayPratice.java
+++ src/com/leetcode/ArrayPratice.java
@@ -1,10 +1,13 @@
 package com.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class ArrayPratice {
 
     //1. Return Odd Number In The Array
+    //O(n)
     private int[] removeEven (int[] arr) {
         int index = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -17,6 +20,7 @@ public class ArrayPratice {
     }
 
     //2. Merge two array and sort it into order
+    //O(m+n)
     public int[] mergeArrays (int[] arr1, int[] arr2) {
         int[] mergedArray = new int[arr1.length + arr2.length];
 
@@ -32,10 +36,131 @@ public class ArrayPratice {
         return mergedArray;
     }
 
+    //3. Two Sum question bruteforce version, better choice is HashTable\
+    // n is the target
+    // O(n^2)
+    public int[] findSum (int[] arr, int n) {
+        int[] result = new int [2];
+
+        for (int i = 0; i < arr.length-1; i++) {
+            int target = n - arr[i];
+            for (int j = i+1; j< arr.length-1; j++) {
+                if (target == arr[j]) {
+                    result[0] = arr[i];
+                    result[1] = target;
+                }
+            }
+        }
+        for(int i= 0; i< result.length; i++) {
+            System.out.print(result[i]);
+        }
+        return result;
+    }
+
+    // 4. Array of Products of all elements except itself(not finished)
+    public int[] findProduct (int arr[]) {
+        int[] result = new int[arr.length];
+        for (int x = 0; x < arr.length-1; x++){
+            result[x] = arr[x];
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (j!=i) {
+                    result[i] = result[i] * arr[j];
+                }
+            }
+        }
+        return result;
+    }
+
+    //5. Find minimum value in array
+    //O(nlogn)
+    public int findMinimum (int[] arr) {
+        Arrays.sort(arr);
+        return arr[0];
+    }
+
+    //6. Find Non-Repeating Integer in an Array
+    //O(n)
+    public int findUnique (int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == arr[i+1]) {
+                i++;
+            } else {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    //7. Find Repeating Integer in an Array
+    // O(n)
+    public int findNonUnique (int[] arr) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i = 0; i < arr.length-1; i++ ) {
+            if (!hs.contains(arr[i])) {
+                hs.add(arr[i]);
+            } else {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    //7. Find First Non-Repeating Integer in an Array
+    // HashMap solution
+    // O(n)
+    public int findFirstUnique (int[] arr) {
+        HashMap<Integer, Integer> countElements = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            countElements.put(arr[i],0);
+        }
+        for(int i = 0; i < arr.length; i++){
+            countElements.put(arr[i],countElements.get(arr[i])+1);
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(countElements.get(arr[i]) <= 1){
+                return arr[i];
+            }
+        }
+        return - 1;
+    }
+
+    //7. Find First Non-Repeating Integer in an Array
+    // Brute Force solution
+    // O(n^2)
+    public int findFirstUnique2(int[] arr) {
+        //Inside Inner Loop Check Each index of outerLoop If it's repeated in array
+        //If it's not repeated then return this as first unique Integer
+        boolean isRepeated = false;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length; j++) {
+
+                if (arr[i] == arr[j] && i != j) {
+                    isRepeated = true;
+                    break;
+                }
+            } //end of InnerLoop
+
+            if (isRepeated == false) {
+                return arr[i];
+            }
+            else {
+                isRepeated = false;
+            }
+
+        } //end of OuterLoop
+        return - 1;
+    }
 
 
     public static void main (String[] args) {
         ArrayPratice ap = new ArrayPratice();
+        int[] arr = {1,21,3,14,5,60,7,6};
+        ap.findSum(arr, 81);
 
     }
 }
