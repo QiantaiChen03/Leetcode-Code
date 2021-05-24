1d372cea319d2cf542b786d0b5d17673f8935e2e
Qiantai <qiantaichen0303@gmail.com>
2021-05-24 15:30:55 -0700

    new array practice

--- src/com/leetcode/AdvancedArrayPractice.java
+++ src/com/leetcode/AdvancedArrayPractice.java
@@ -1,4 +1,28 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 public class AdvancedArrayPractice {
+
+    // 2. Search a Rotated Array
+    public int binarySearchRotated (int[] arr, int key) {
+        int high = arr.length;
+        int low = 0;
+        Arrays.sort(arr);
+
+        binarySearch(arr, key, low, high);
+
+        return -1;
+    }
+    public int binarySearch (int[] arr, int key, int low, int high) {
+        return -1;
+    }
+
+
+    public static void main (String[] args){
+        AdvancedArrayPractice aap = new AdvancedArrayPractice();
+
+    }
+
+
 }
