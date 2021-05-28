b7bf69166598687d3aea6ade0557012c4e2be6c6
Qiantai <qiantaichen0303@gmail.com>
2021-05-28 14:37:03 -0700

    algorithm

--- src/AlgorithmPractice/DijkstraAlgo.java
+++ src/AlgorithmPractice/DijkstraAlgo.java
@@ -88,7 +88,7 @@ public class DijkstraAlgo {
     // Driver code
     public static void main(String arg[])
     {
-        int V = 11;
+        int V = 8;
         int source = 0;
 
         // Adjacency list representation of the
@@ -102,113 +102,48 @@ public class DijkstraAlgo {
         }
 
         // Inputs for the DPQ graph
-//        //A 0
-//        adj.get(0).add(new Node(1, 12));
-//        adj.get(0).add(new Node(2, 3));
-//        adj.get(0).add(new Node(3, 7));
-//        // B 1
-//        adj.get(1).add(new Node(0, 12));
-//        adj.get(1).add(new Node(2, 8));
-//        adj.get(1).add(new Node(4, 15));
-//        //C 2
-//        adj.get(2).add(new Node(0, 3));
-//        adj.get(2).add(new Node(1, 8));
-//        adj.get(2).add(new Node(3, 6));
-//        adj.get(2).add(new Node(4, 5));
-//        adj.get(2).add(new Node(5, 2));
-//        adj.get(2).add(new Node(6, 4));
-//        //D 3
-//        adj.get(3).add(new Node(0, 7));
-//        adj.get(3).add(new Node(2, 6));
-//        adj.get(3).add(new Node(6, 1));
-//        //E 4
-//        adj.get(4).add(new Node(1, 15));
-//        adj.get(4).add(new Node(2, 5));
-//        adj.get(4).add(new Node(5, 6));
-//        adj.get(4).add(new Node(7, 9));
-//        adj.get(4).add(new Node(8, 10));
-//        //F 5
-//        adj.get(5).add(new Node(2, 2));
-//        adj.get(5).add(new Node(4, 6));
-//        adj.get(5).add(new Node(6, 6));
-//        adj.get(5).add(new Node(7, 10));
-//        //G 6
-//        adj.get(6).add(new Node(2, 4));
-//        adj.get(6).add(new Node(3, 1));
-//        adj.get(6).add(new Node(4, 6));
-//        adj.get(6).add(new Node(7, 6));
-//        adj.get(6).add(new Node(9, 9));
-//        //H 7
-//        adj.get(7).add(new Node(4, 9));
-//        adj.get(7).add(new Node(5, 10));
-//        adj.get(7).add(new Node(6, 6));
-//        adj.get(7).add(new Node(8, 2));
-//        adj.get(7).add(new Node(9, 5));
-//        //I 8
-//        adj.get(8).add(new Node(4, 10));
-//        adj.get(8).add(new Node(7, 2));
-//        adj.get(8).add(new Node(9, 12));
-//        //J 9
-//        adj.get(9).add(new Node(6, 9));
-//        adj.get(9).add(new Node(7, 5));
-//        adj.get(9).add(new Node(8, 12));
-
         //A 0
-        adj.get(0).add(new Node(1, 22));
-        adj.get(0).add(new Node(4, 8));
-        adj.get(0).add(new Node(7, 14));
+        adj.get(0).add(new Node(1, 20));
+        adj.get(0).add(new Node(2, 24));
+        adj.get(0).add(new Node(3, 4));
         // B 1
-        adj.get(1).add(new Node(0, 22));
-        adj.get(1).add(new Node(2, 29));
-        adj.get(1).add(new Node(4, 19));
+        adj.get(1).add(new Node(0, 20));
+        adj.get(1).add(new Node(2, 23));
+        adj.get(1).add(new Node(5, 21));
         //C 2
-        adj.get(2).add(new Node(1, 29));
-        adj.get(2).add(new Node(4, 26));
-        adj.get(2).add(new Node(5, 7));
-        adj.get(2).add(new Node(6, 18));
-        adj.get(2).add(new Node(3, 24));
+        adj.get(2).add(new Node(0, 21));
+        adj.get(2).add(new Node(1, 23));
+        adj.get(2).add(new Node(3, 29));
+        adj.get(2).add(new Node(4, 8));
+        adj.get(2).add(new Node(5, 1));
+        adj.get(2).add(new Node(6, 15));
         //D 3
-        adj.get(3).add(new Node(2, 24));
-        adj.get(3).add(new Node(6, 23));
-        adj.get(3).add(new Node(10, 3));
+        adj.get(3).add(new Node(0, 4));
+        adj.get(3).add(new Node(2, 29));
+        adj.get(3).add(new Node(6, 7));
         //E 4
-        adj.get(4).add(new Node(0, 8));
-        adj.get(4).add(new Node(1, 19));
-        adj.get(4).add(new Node(2, 26));
-        adj.get(4).add(new Node(5, 30));
-        adj.get(4).add(new Node(7, 12));
-        adj.get(4).add(new Node(8, 1));
+        adj.get(4).add(new Node(1, 21));
+        adj.get(4).add(new Node(2, 8));
+        adj.get(4).add(new Node(5, 13));
+        adj.get(4).add(new Node(7, 5));
         //F 5
-        adj.get(5).add(new Node(2, 7));
-        adj.get(5).add(new Node(4, 30));
-        adj.get(5).add(new Node(6, 15));
-        adj.get(5).add(new Node(8, 9));
+        adj.get(5).add(new Node(2, 1));
+        adj.get(5).add(new Node(4, 13));
+        adj.get(5).add(new Node(6, 10));
+        adj.get(5).add(new Node(7, 3));
         //G 6
-        adj.get(6).add(new Node(3, 23));
-        adj.get(6).add(new Node(2, 18));
-        adj.get(6).add(new Node(5, 15));
-        adj.get(6).add(new Node(8, 11));
-        adj.get(6).add(new Node(9, 6));
-        adj.get(6).add(new Node(10, 27));
+        adj.get(6).add(new Node(2, 15));
+        adj.get(6).add(new Node(3, 7));
+        adj.get(6).add(new Node(5, 10));
+        adj.get(6).add(new Node(7, 18));
         //H 7
-        adj.get(7).add(new Node(0, 14));
-        adj.get(7).add(new Node(4, 12));
-        adj.get(7).add(new Node(8, 28));
+        adj.get(7).add(new Node(4, 5));
+        adj.get(7).add(new Node(5, 3));
+        adj.get(7).add(new Node(6, 18));
 
-        //I 8
-        adj.get(8).add(new Node(4, 1));
-        adj.get(8).add(new Node(5, 9));
-        adj.get(8).add(new Node(6, 11));
-        adj.get(8).add(new Node(7, 28));
-        adj.get(8).add(new Node(9, 25));
-        //J 9
-        adj.get(9).add(new Node(6, 6));
-        adj.get(9).add(new Node(10, 5));
-        adj.get(9).add(new Node(8, 25));
-        //K 10
-        adj.get(10).add(new Node(3, 3));
-        adj.get(10).add(new Node(6, 27));
-        adj.get(10).add(new Node(9, 5));
+
+
+
 
         // Calculate the single source shortest path
         DijkstraAlgo dpq = new DijkstraAlgo(V);
