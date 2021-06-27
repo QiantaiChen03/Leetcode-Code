2530d74a6eeed2a6f49c47642ccf68bd45091f75
Qiantai <qiantaichen0303@gmail.com>
2021-06-27 00:07:55 -0700

    algorithm

--- src/com/leetcode/BinarySearch/Q35.java
+++ src/com/leetcode/BinarySearch/Q35.java
@@ -8,19 +8,17 @@ public class Q35 {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left < right) {
+        while (left <= right) {
             int medium = left + (right - left) / 2;
             if (target < nums[medium]) {
                 right = medium - 1;
             } else if (target > nums[medium]) {
                 left = medium + 1;
-            } else if (target == nums[medium]) {
+            } else {
                 return medium;
             }
-
         }
-
-        return -1;
+        return left;
     }
 
 }
