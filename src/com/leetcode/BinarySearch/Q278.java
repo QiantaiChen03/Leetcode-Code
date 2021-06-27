25eb1eb2d525b2fc5b57262652235c7e7f203278
Qiantai <qiantaichen0303@gmail.com>
2021-06-27 00:32:10 -0700

    algorithm

--- src/com/leetcode/BinarySearch/Q278.java
+++ src/com/leetcode/BinarySearch/Q278.java
@@ -1,4 +1,22 @@
 package com.leetcode.BinarySearch;
 
 public class Q278 {
+    // First Bad Version
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            int medium = left + (right - left) / 2;
+            if (!isBadVersion(medium)) {
+                left = medium + 1;
+            } else {
+                right = medium-1;
+            }
+        }
+        return left;
+    }
+
+    public boolean isBadVersion(int version) {
+        return false;
+    }
 }
